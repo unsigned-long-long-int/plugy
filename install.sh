2acce5661ed6dd9212e55f6d828c0ef75bcbfe1c
redacted <121198893+unsigned-long-long-int@users.noreply.github.com>
2025-06-24 16:36:28 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 mkdir -p ~/.local/bin ~/.config/plugy/plugins
 wget -O ~/.local/bin/plugy https://raw.githubusercontent.com/unsigned-long-long-int/plugy/refs/heads/main/plugy
 chmod +x ~/.local/bin/plugy
-echo "Pluggy has been installed at ~/.local/bin/plugy. Ensure it is present in \$PATH"
+echo "Plugy has been installed at ~/.local/bin/plugy. Ensure it is present in \$PATH"
