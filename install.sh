b04a77133e9c3c1479956ec3b7e681aa903ff4ef
redacted <121198893+unsigned-long-long-int@users.noreply.github.com>
2025-06-24 16:18:50 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ git clone https://github.com/unsigned-long-long-int/plugy.git
 mkdir -p ~/.local/bin ~/.config/plugy/plugins
 cp plugy/plugy ~/.local/bin/plugy
 chmod +x ~/.local/bin/plugy
-echo "Pluggy has been installed at ~/.local/bin/plugy. Ensure it is present in $PATH"
+echo "Pluggy has been installed at ~/.local/bin/plugy. Ensure it is present in \$PATH"
